6d520220d654d3a5b2836bc353f46b1c08e443e7
Mrudul Patil <mrudulsp.18@gmail.com>
2022-12-09 16:13:32 +0530

    reset button added

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 
 const container = document.querySelector('.container');
 
-for(let i = 1; i<=16; i++){
-    let line = document.createElement('div');
-    line.classList.add(`line`);
-    for(let j = 1; j <=16; j++){
-        let coloumn = document.createElement('div');
-        coloumn.classList.add(`coloum`);
-        line.appendChild(coloumn);
-    }
-    container.appendChild(line);
-}
-
-function hover(){
-    this.classList.add('hover');
-}
 
 function covered(e){
-    this.classList.remove('hover');
     this.classList.add('covered');
 }
 
-const lines = document.querySelectorAll('.line');
-lines.forEach(line=> {
-    
-})
+function createGrid(l){
+    let a = `${500/l}px`;
+    for(let i = 1; i<=l; i++){
+        let line = document.createElement('div');
+        line.classList.add(`line`);
+        for(let j = 1; j <=l; j++){
+            let coloumn = document.createElement('div');
+            coloumn.style.width = a;
+            coloumn.style.height = a;
+            coloumn.classList.add(`coloum`);
+            line.appendChild(coloumn);
+        }
+        container.appendChild(line);
+    }
+    const boxes = document.querySelectorAll('.coloum');
+    boxes.forEach(box=> {
+        box.addEventListener('mouseover', covered);
+    })
 
-const boxes = document.querySelectorAll('.coloum');
-boxes.forEach(box=> {
-    box.addEventListener('mouseover', covered);
-})
+}
 
+
+
+function reset(){
+    const lines = document.querySelectorAll('.line');
+    const boxes = document.querySelectorAll('.coloum');
+    boxes.forEach(box=> {
+        box.remove();
+    })
+    lines.forEach(line=> {
+        line.remove();
+    })
+    let width = 1000;
+    while(width > 100 || width < 1){
+        width = prompt("Enter Grid Size", "eg. 20");
+    }
+    createGrid(width);
+}
+
+createGrid(16);
