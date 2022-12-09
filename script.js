ac3fc5bf27a9db53a299e447edaca8cf73a197a0
Mrudul Patil <mrudulsp.18@gmail.com>
2022-12-09 17:20:00 +0530

    styling, rgb added

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 
+let control = 0;
 const container = document.querySelector('.container');
+
+function control0(){
+    control = 0;
+}
+function control1(){
+    control = 1;
+}
 
 
 function covered(e){
-    this.classList.add('covered');
+    if(control == 0){
+        this.style.background = 'black';
+    }
+    else{
+        let r = Math.random() * 254 + 1;let g = Math.random() * 254 + 1;let b = Math.random() * 254 + 1;
+        this.style.background = `rgb(${r}, ${g}, ${b})`;
+    }
 }
 
 function createGrid(l){
